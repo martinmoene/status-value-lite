965350a65f70acc39fabdee134d0ae81ce734a81
Martin Moene <martin.moene@gmail.com>
2020-12-28 13:53:20 +0100

    Change whitespace

--- include/nonstd/status_value_cpp98.hpp
+++ include/nonstd/status_value_cpp98.hpp
@@ -31,7 +31,7 @@
 
 #ifndef nsstsv_CONFIG_NO_EXCEPTIONS
 # if _MSC_VER
-# include <cstddef>     // for _HAS_EXCEPTIONS
+#  include <cstddef>    // for _HAS_EXCEPTIONS
 # endif
 # if defined(__cpp_exceptions) || defined(__EXCEPTIONS) || (_HAS_EXCEPTIONS)
 #  define nsstsv_CONFIG_NO_EXCEPTIONS  0
