6a9ef38f37847ba2dc5f8e7d8feee22a86bbc108
Martin Moene <martin.moene@gmail.com>
2021-07-05 17:00:48 +0200

    Handle lest test framework as system include to prevent warnings

--- test/t.bat
+++ test/t.bat
@@ -35,8 +35,8 @@ set msvc_defines=^
 
 set CppCoreCheckInclude=%VCINSTALLDIR%\Auxiliary\VS\include
 
-::cl -W3 -EHsc %std% %unit_select% %unit_config% %msvc_defines% -I"%CppCoreCheckInclude%" -I../include %unit_file%-main.t.cpp %unit_file%.t.cpp && %unit_file%-main.t.exe
-cl -W3 -EHsc %std% %unit_select% %unit_config% %msvc_defines% -I"%CppCoreCheckInclude%" -I../include %unit_file%.t.cpp && %unit_file%.t.exe
+::cl -nologo -W3 -EHsc %std% %unit_select% %unit_config% %msvc_defines% -I"%CppCoreCheckInclude%" -Ilest -I../include -I. %unit_file%-main.t.cpp %unit_file%.t.cpp && %unit_file%-main.t.exe
+cl -nologo -W3 -EHsc %std% %unit_select% %unit_config% %msvc_defines% -I"%CppCoreCheckInclude%" -Ilest -I../include -I. %unit_file%.t.cpp && %unit_file%.t.exe
 endlocal & goto :EOF
 
 :: subroutines:
