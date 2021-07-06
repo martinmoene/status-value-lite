f255dc704e6e5d7d5dfa96f05fce9e1ef617d185
Martin Moene <martin.moene@gmail.com>
2021-07-06 17:46:59 +0200

    Fix script for lest auto registration, executable filename

--- test/tc-cl.bat
+++ test/tc-cl.bat
@@ -26,6 +26,7 @@ call :CompilerVersion version
 echo %clang% %version%: %std% %unit_select% %args%
 
 set unit_config=^
+    -Dlest_FEATURE_AUTO_REGISTER=1 ^
     -D%unit%_%UCAP%_HEADER=\"nonstd/%unit%.hpp\" ^
     -D%unit%_TEST_NODISCARD=0 ^
     -D%unit%_CONFIG_SELECT_%UCAP%=%unit_select%
@@ -35,7 +36,7 @@ set  optflags=-O2
 set warnflags=-Wall -Wextra -Wpedantic -Weverything -Wshadow -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-padded -Wno-missing-noreturn -Wno-documentation-unknown-command -Wno-documentation-deprecated-sync -Wno-documentation -Wno-weak-vtables -Wno-missing-prototypes -Wno-missing-variable-declarations -Wno-exit-time-destructors -Wno-global-constructors -Wno-sign-conversion -Wno-sign-compare -Wno-implicit-int-conversion -Wno-deprecated-declarations -Wno-date-time
 
 ::"%clang%" -EHsc -std:%std% %optflags% %warnflags% %unit_config% -fms-compatibility-version=19.00 /imsvc lest -I../include -Ics_string -I. -o %unit_file%-main.t.exe %unit_file%-main.t.cpp %unit_file%.t.cpp && %unit_file%-main.t.exe
-"%clang%" -EHsc -std:%std% %optflags% %warnflags% %unit_config% -fms-compatibility-version=19.00 /imsvc lest -I../include -Ics_string -I. -o %unit_file%-main.t.exe %unit_file%.t.cpp && %unit_file%.t.exe
+"%clang%" -EHsc -std:%std% %optflags% %warnflags% %unit_config% -fms-compatibility-version=19.00 /imsvc lest -I../include -Ics_string -I. -o %unit_file%.t.exe %unit_file%.t.cpp && %unit_file%.t.exe
 endlocal & goto :EOF
 
 :: subroutines:
